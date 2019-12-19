c404d9f4d21d3f524a7a210bedad193a429c4ce0
yangmf <yangmf@localhost.localdomain>
2019-12-19 06:28:19 +0000

    天气

--- app/Http/Controllers/WX/WXController.php
+++ app/Http/Controllers/WX/WXController.php
@@ -120,7 +120,23 @@ class WXController extends Controller
                 echo $response_text;        //回复用户消息
             }
         }elseif($event=='CLICK'){
-            echo 222;die;
+            //如果是 获取天气
+            //请求第三方接口 获取天气
+            $weather_api = 'https://free-api.heweather.net/s6/weather/now?location=beijing&key=d957029d5931428f8eef6ba241aefdd7';
+            $weather_info = file_get_contents($weather_api);
+            $weather_info_arr = json_decode($weather_info,true);
+            $cond_txt = $weather_info_arr['HeWeather6'][0]['now']['cond_txt'];
+            $tmp = $weather_info_arr['HeWeather6'][0]['now']['tmp'];
+            $wind_dir = $weather_info_arr['HeWeather6'][0]['now']['wind_dir'];
+            $msg = $cond_txt . ' 温度： '.$tmp . ' 风向： '. $wind_dir;
+            $response_xml = '<xml>
+                <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
+                <CreateTime>'.time().'</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA['. date('Y-m-d H:i:s') .  $msg .']]></Content>
+                </xml>';
+            echo $response_xml;
         }
 
         //判断消息类型
