da8763c89927307fade4bfc714fd73c70273dea5
yangmf <yangmf@localhost.localdomain>
2019-12-20 02:55:35 +0000

    微信

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -8,14 +8,13 @@ use Illuminate\Support\Facades\Redis;
 class VoteController extends Controller
 {
     public function index(){
-        echo '<pre>';print_r($_GET);echo '</pre>';
+//        echo '<pre>';print_r($_GET);echo '</pre>';
 
         $code = $_GET['code'];
         //获取access_token
         $data = $this->getAccessToken($code);
 
         echo '<pre>';print_r($data);echo '</pre>';
-        echo 222;
     }
 
 
@@ -25,7 +24,6 @@ class VoteController extends Controller
     protected function getAccessToken($code)
     {
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('appid').'&secret='.env('secret').'&code='.$code.'&grant_type=authorization_code';
-        echo $url;die;
         $json_data = file_get_contents($url);
         return json_decode($json_data,true);
     }
