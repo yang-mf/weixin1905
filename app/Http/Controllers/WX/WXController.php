514145085416f0bd17f927aaae607f3b542ab088
yangmf <yangmf@localhost.localdomain>
2019-12-20 00:27:46 +0000

    微信

--- app/Http/Controllers/WX/WXController.php
+++ app/Http/Controllers/WX/WXController.php
@@ -282,7 +282,7 @@ class WXController extends Controller
                 [
                     'type'  => 'view',
                     'name'  => '投票',
-                    'url'   =>'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9458fefe0c30d65b&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=WX1905#wechat_redirect',
+                    'url'   =>'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9458fefe0c30d65b&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state=WX1905#wechat_redirect',
                 ],
             ]
         ];
